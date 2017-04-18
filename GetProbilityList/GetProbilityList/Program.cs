26d8e744315597777eee6f09535644ea9bcbb293
嘉诚 卢 <lujiacheng45@hotmail.com>
2017-04-19 01:54:17 +0800

    1.将读取数据整理为道具名、道具数量、概率3个列表 2.将列表排版输出至txt

--- GetProbilityList/GetProbilityList/Program.cs
+++ GetProbilityList/GetProbilityList/Program.cs
@@ -3,43 +3,69 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Text;
+using NPOI.SS.Formula.Functions;
 
 namespace GetProbilityList
 {
-    class Program
+    internal class Program
     {
         public static string FilePath { get; set; }
         public static DataTable DataTableAward { get; set; }
         public static DataTable DataTableKu { get; set; }
-        public static readonly string[] Name1 = {"必给物品1类ID","必给物品2类ID", "必给物品3类ID", "必给物品4类ID" };
-        public static readonly string[] Name2 = {"物品1类", "物品2类", "物品3类", "物品4类", "物品5类", "物品6类", "物品7类", "物品8类" };
-        public static readonly string[] Number1 = {"必给物品1类数量","必给物品2类数量", "必给物品3类数量", "必给物品4类数量" };
-        public static readonly string[] Number2 = {"物品1类数量", "物品2类数量", "物品3类数量", "物品4类数量", "物品5类数量", "物品6类数量", "物品7类数量", "物品8类数量" };
-        public static readonly string[] Problity = {"物品1类掉率", "物品2类掉率", "物品3类掉率", "物品4类掉率", "物品5类掉率", "物品6类掉率", "物品7类掉率", "物品8类掉率" };
+        public static readonly string[] Name1 = { "必给物品1类ID", "必给物品2类ID", "必给物品3类ID", "必给物品4类ID" };
+        public static readonly string[] Name2 = { "物品1类", "物品2类", "物品3类", "物品4类", "物品5类", "物品6类", "物品7类", "物品8类" };
+        public static readonly string[] Number1 = { "必给物品1类数量", "必给物品2类数量", "必给物品3类数量", "必给物品4类数量" };
+        public static readonly string[] Number2 = { "物品1类数量", "物品2类数量", "物品3类数量", "物品4类数量", "物品5类数量", "物品6类数量", "物品7类数量", "物品8类数量" };
+        public static readonly string[] Problity = { "物品1类掉率", "物品2类掉率", "物品3类掉率", "物品4类掉率", "物品5类掉率", "物品6类掉率", "物品7类掉率", "物品8类掉率" };
+        public static List<string> NameList = new List<string>();//名字列表
+        public static List<string> NumberList = new List<string>();//数量列表
+        public static List<string> ProbilityList = new List<string>();//概率列表
+        public static List<int> HideRowList = new List<int>();//被调用的奖励列表，最后统计时不会显示
+        public static float ProbilitySum { get; set; }//概率和
 
-        static void Main()
+        private static void Main()
         {
             //读表
             Program program = new Program();
             program.InitializeConfig();
-            
+
             //获取每行的数据
+            if (DataTableAward == null) return;
+
+            //写入文件
+            FileStream fs = new FileStream(GetAppConfig(nameof(FilePath)) + @"\1.txt", FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+
             for (int i = 0; i < DataTableAward.Rows.Count; i++)
             {
-                var nameList = GetNameList(i);
+                ProbilitySum = Convert.ToInt32(DataTableAward.Rows[i]["总概率"]);
+                GetList(i, out NameList, out NumberList, out ProbilityList);
 
-                Console.WriteLine(DataTableAward.Rows[i]["id说明"]);
-                foreach (string t in nameList)
+                if (HideRowList.Contains(i))
                 {
-                    Console.WriteLine(t);
+                    continue;
                 }
+
+                //开始写入
+                Console.WriteLine($"执行到第{i}行");
+                sw.WriteLine(DataTableAward.Rows[i]["id说明"]);
+                for (int j = 0; j < NameList.Count; j++)
+                {
+                    sw.WriteLine(NameList[j] + "    " + NumberList[j] + "    " + ProbilityList[j]);
+                }
+                sw.WriteLine();
             }
+            //清空缓冲区
+            sw.Flush();
+            //关闭流
+            sw.Close();
+            fs.Close();
+            Console.WriteLine("开饭啦！");
             Console.ReadKey();
-
-
-
         }
 
         //从表里读奖励和库
@@ -58,13 +84,6 @@ namespace GetProbilityList
             ExcelHelper excelHelper = new ExcelHelper(FilePath);
             DataTableAward = excelHelper.ExcelToDataTable("Sheet1", true);
             DataTableKu = excelHelper.ExcelToDataTable("Sheet2", true);
-
-            
-            //int count = excelHelper.DataTableToExcel(data, "Sheet1", true);
-            //if (count > 0)
-            //    Console.WriteLine("Number of imported data is {0} ", count);
-
-            //Console.ReadKey();
         }
 
         //读FilePath
@@ -76,7 +95,7 @@ namespace GetProbilityList
         }
 
         //获取List
-        private static void GetNameList(int row,out List<string> nameList, out List<string> numberList, out List<string> probilityList)
+        private static void GetList(int row, out List<string> nameList, out List<string> numberList, out List<string> probilityList)
         {
             nameList = new List<string>();
             numberList = new List<string>();
@@ -93,15 +112,28 @@ namespace GetProbilityList
             //随机道具
             for (int i = 0; i < 8; i++)
             {
-                if (GetData(row, Name2[i], out string str, out int kuId) == 2)
+                if (GetData(row, Name2[i], out string str, out int awardId) == 3)
                 {
-                    nameList.Add(GetItemName(kuId.ToString()));
-                    numberList.Add(DataTableAward.Rows[row][Number2[i]].ToString());
-                    probilityList.Add("1");
+                    //记录不显示的row
+                    HideRowList.Add(GetRow(awardId.ToString()));
+                    GetList(GetRow(awardId.ToString()), out List<string> nameList2, out List<string> numberList2,
+                        out List<string> probilityList2);
+                    nameList.AddRange(nameList2);
+                    numberList.AddRange(numberList2);
+                    probilityList.AddRange(probilityList2);
+                    break;
                 }
-                else if (GetData(row, name, out str, out int awardId) == 3)
+                if (GetData(row, Name2[i], out str, out int kuId) != 2) continue;
+                nameList.Add(GetItemName(kuId.ToString()));
+                numberList.Add(DataTableAward.Rows[row][Number2[i]].ToString());
+
+                if (GetData(row, Problity[i], out string probilityStr, out int probilityNum) == 3 || GetData(row, Problity[i], out probilityStr, out probilityNum) == 2)
                 {
-                    nameList.AddRange(GetNameList(Convert.ToInt32(GetRow(awardId.ToString()))));
+                    probilityList.Add((probilityNum / ProbilitySum).ToString(CultureInfo.InvariantCulture));
+                }
+                else
+                {
+                    probilityList.Add(probilityStr + "//" + DataTableAward.Rows[row]["总概率"]);
                 }
             }
         }
@@ -113,9 +145,17 @@ namespace GetProbilityList
         }
 
         //根据奖励id查找row
-        private static string GetRow(string awardId)
+        private static int GetRow(string awardId)
         {
-            return (from DataRow row in DataTableAward.Rows where row["id"].ToString() == awardId select row["id"].ToString()).FirstOrDefault();
+            try
+            {
+                return Convert.ToInt32((from DataRow row in DataTableAward.Rows where row["id"].ToString() == awardId select DataTableAward.Rows.IndexOf(row)).FirstOrDefault());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + $"awardId:{awardId}");
+                throw;
+            }
         }
 
         //0为空或0
@@ -140,12 +180,5 @@ namespace GetProbilityList
             num = 0;
             return 1;
         }
-
-        private static double GetProbility(int row, string weigh)
-        {
-        }
-
-
-
     }
 }
